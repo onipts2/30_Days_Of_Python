77c1d7c470e6b9bd7c7673a8e46a7e199cc22b83
onipsts2 <dvt2k6.it@gmail.com>
2024-11-10 14:36:21 +0700

    init 30DaysOfPython

---
+++ day_1.py
@@ -0,0 +1,21 @@
+# Toán tử
+print(3 + 2)   # phép cộng
+print(3 - 2)   # phép trừ
+print(3 * 2)   # phép nhân
+print(3 / 2)   # phép chia 
+print(3 ** 2)  # hàm mũ 
+print(3 % 2)   # chia lấy phần dư 
+print(3 // 2)  # chia lấy phần nguyên 
+
+# Kiểm tra kiểu dữ liệu 
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(3 == 3))              # Bool
+print(type(3 >= 3))              # Bool
